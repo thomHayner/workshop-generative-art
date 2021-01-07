36baa0385ba3822c6506fb62f3e867435754939b
thomHayner <thom.hayner@gmail.com>
2021-01-07 04:07:11 -0700

    connect 2 random points, make trapeziod with straight lines to botom

--- src/2d/wallDrawing.js
+++ src/2d/wallDrawing.js
@@ -1,15 +1,15 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
 const sketch = () => {
-  const gridSize = 6
-
+  
   const gridBuilder = () => {
     const points = [];
-
+    
     for (let x = 0; x < gridSize; x++) {
       for (let y = 0; y < gridSize; y++) {
         const u = (x / (gridSize - 1));
@@ -17,24 +17,54 @@ const sketch = () => {
         points.push([ u, v ])
       }
     };
+
     return points
   }
   
+  const rondomPointSelector = (arr) => {
+    const randomizedPoints = [];
+    const tempArr = random.shuffle(arr);
+
+    for (let x = 0; x < arr.length / 2; x++) {
+      randomizedPoints.push([ tempArr.shift(), tempArr.pop()])
+    }
+    return randomizedPoints
+  }
+  
+  const gridSize = 6
   const points = gridBuilder();
-  const margin = 400;
+  const randomizedPoints = rondomPointSelector(points)
+
+  console.log(randomizedPoints)
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     points.forEach(([ u, v ]) => {
-      const x = (u * (width - margin)) + (margin * 0.5);
-      const y = v * (height - margin) + (margin * 0.5);
+      const x = u * (width - (width / gridSize)) + ((width / gridSize) * 0.5);
+      const y = v * (height - (width / gridSize)) + ((width / gridSize) * 0.5);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
+      context.arc(x, y, 10, 0, Math.PI * 2, false);
       context.fillStyle = "black";
       context.fill()
+    })
+
+    randomizedPoints.forEach(([[ s, t ], [ u, v ]]) => {
+      const x1 = s * (width - (width / gridSize)) + ((width / gridSize) * 0.5);
+      const y1 = t * (height - (width / gridSize)) + ((width / gridSize) * 0.5);
+      const x2 = u * (width - (width / gridSize)) + ((width / gridSize) * 0.5);
+      const y2 = v * (height - (width / gridSize)) + ((width / gridSize) * 0.5);
+
+      context.beginPath();
+      context.lineWidth = 5;
+      context.moveTo(x1, (width / gridSize) * 0.5);
+      context.lineTo(x1, y1);
+      context.lineTo(x2, y2);
+      context.lineTo(x2, width - ((width / gridSize) * 0.5));
+      context.lineTo(x1, width - ((width / gridSize) * 0.5));
+      context.stroke();
     })
   }
 };
