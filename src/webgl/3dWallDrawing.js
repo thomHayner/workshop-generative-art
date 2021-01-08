6f0144e86cc0c4820ee3498a61421614eff01842
thomHayner <thom.hayner@gmail.com>
2021-01-08 12:22:00 -0700

    built saul lewitt wall drawing 1971 boston museum in 3d

---
+++ src/webgl/3dWallDrawing.js
@@ -0,0 +1,103 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+
+const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl"
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas
+  });
+
+  // WebGL background color
+  renderer.setClearColor("#fff", 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+  camera.position.set(0, 0, -4);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // Setup a geometry
+  const geometry = new THREE.BoxGeometry(.010, .010, .010);
+
+  // Setup a material
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    wireframe: false,
+  });
+
+  const pointsCoords = []
+
+  // Setup a mesh with geometry + material
+  for (let i = 0; i < 50; i++) {
+    const pointCube = new THREE.Mesh(geometry, material);
+    pointCube.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1),
+    );
+    pointsCoords.push(pointCube.position);
+    scene.add(pointCube);
+  }
+
+  for (let i = 0; i < pointsCoords.length; i++) {
+    const startPoint = pointsCoords[i];
+
+    for (let j = 0; j < pointsCoords.length; j++) {
+      const endPoint = pointsCoords[j];
+      const points = [];
+      points.push(startPoint);
+      points.push(endPoint);
+
+      const lineMaterial = new THREE.LineBasicMaterial({
+        color: 0x000000,
+        linewidth: 0.05,
+      });
+      const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
+
+      const line = new THREE.Line( lineGeometry, lineMaterial );
+      scene.add( line );
+    }
+  }
+
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      controls.update();
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
