7434b4a1f834c38363b8e8952d9aea97eae889f7
thomHayner <thom.hayner@gmail.com>
2021-01-19 18:28:05 -0700

    lesson complete

--- src/webgl/shaderLesson.js
+++ src/webgl/shaderLesson.js
@@ -9,15 +9,40 @@ const settings = {
 };
 
 // Your glsl code
-const frag = glsl(`
+const frag = glsl(/* glsl */`
   precision highp float;
 
   uniform float time;
+  uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
+
   void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
+
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
+
+    float dist = length(center);
+
+    float alpha = smoothstep(0.252, 0.25, dist);
+
+    float n = noise(vec3(center * 1.0, time));
+
+    vec3 color = hsl2rgb(
+      n * 0.2 + 0.6,
+      0.5,
+      0.5
+    );
+
+    gl_FragColor = vec4(color, alpha);
+    
+    // vec3 colorA = sin(time) + vec3(0.0, 0.0, 1.0);
+    // vec3 colorB = vec3(0.0, 1.0, 0.0);
+
+    // vec3 color = mix(colorA, colorB, vUv.x * sin(time) + vUv.y);
+    // gl_FragColor = vec4(color, alpha);
   }
 `);
 
@@ -25,6 +50,7 @@ const frag = glsl(`
 const sketch = ({ gl }) => {
   // Create the shader and return it
   return createShader({
+    clearColor: 'white',
     // Pass along WebGL context
     gl,
     // Specify fragment and/or vertex shader strings
@@ -32,7 +58,8 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time
+      time: ({ time }) => time,
+      aspect: ({ width, height }) => width / height,
     }
   });
 };
